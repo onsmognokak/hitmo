ab952de1f2f77bc9f82f3f1e840c41513b351ede
onsmognokak <111427218+onsmognokak@users.noreply.github.com>
2022-12-05 11:15:04 +0300

    Update move_mp3.py

--- move_mp3.py
+++ move_mp3.py
@@ -1,12 +1,5 @@
 #python 3.11
 
-#просмотреть все файлы
-#выбрать endswith(.mp3)
-#переместить выбранные
-#replace '_' на ' '
-#удалить код hitmos по индексу с конца строки [-12:-4] если все это числа
-#cnt == 8
-
 import os
 
 cnt = 0
@@ -22,13 +15,7 @@ for _, _, filenames in os.walk(downloads_folder):
                 if cnt >= 8:
                     new_filename = filename[:-13]+filename[-4:]
                     new_filename = new_filename.replace("_", " ")
-                    #os.rename(downloads_folder + filename, new_filename)
                     os.replace(downloads_folder + filename, music_folder + new_filename)
                     os.exit()
             os.replace(downloads_folder + filename, music_folder + filename)
-
-
-
-
-
 
